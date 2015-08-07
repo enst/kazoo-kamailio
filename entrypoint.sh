a74d61b577a6f9c22b47bcd4777718171b444e36
Bing Li <enst.bupt@gmail.com>
2015-08-07 14:24:12 -0400

    dbtext/dispatcher

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+MY_IP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
+sed -i "/1 sip:/s/127.0.0.1/$MY_IP/" /etc/kazoo/kamailio/dbtext/dispatcher
+
 service kamailio start
 
 exec /bin/bash "$@"
