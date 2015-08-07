ebd98268e398704b310844a10d76db4e9ebe6b24
root <root@debian.privatedns.com>
2015-08-07 19:15:29 -0400

    sed typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 MY_IP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
-sed -i '/1 sip:/s/127.0.0.1/'$MY_IP'/g'' /etc/kazoo/kamailio/dbtext/dispatcher
+sed -i '/1 sip:/s/127.0.0.1/'$MY_IP'/g' /etc/kazoo/kamailio/dbtext/dispatcher
 sed -i '/MY_IP_ADDRESS/s/127.0.0.1/'$MY_IP'/g' /etc/kazoo/kamailio/local.cfg
-sed -i 'MY_HOSTNAME/s/kamailio.2600hz.com/'$HOSTNAME'/g' /etc/kazoo/kamailio/local.cfg
+sed -i '/MY_HOSTNAME/s/kamailio.2600hz.com/'$HOSTNAME'/g' /etc/kazoo/kamailio/local.cfg
 
 service kamailio start
 
